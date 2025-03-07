e505263c068e0ebc51c10b290b9656b2517970e7
ParkSY0919 <qhr498@gmail.com>
2025-03-07 17:22:34 +0900

    [Feat/#3] 탭바 구현 및 폴더링 수정

---
+++ Het-Je-iOS/Het-Je-iOS/Presentation/1. 탭바(TabBar)/Views/TabBarViewController.swift
@@ -0,0 +1,80 @@
+//
+//  TabBarViewController.swift
+//  Het-Je-iOS
+//
+//  Created by 박신영 on 3/7/25.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    
+    private let exchangeVC = UINavigationController(rootViewController: OtherViewController())
+    private let coninInfoVC = UINavigationController(rootViewController: OtherViewController())
+    private let portfolioVC = UINavigationController(rootViewController: OtherViewController())
+    
+    override func viewDidLoad() {
+        super.viewDidLoad ()
+        
+        setDelegate()
+        setStyle()
+        setTabBarAppearence()
+    }
+    
+    private func setDelegate() {
+        self.delegate = self
+    }
+    
+    private func setStyle() {
+        exchangeVC.do {
+            $0.tabBarItem = UITabBarItem(title: StringLiterals.TabBar.exchangeTitle,
+                                         image: AppIconType.uptrend_Chart.image,
+                                         selectedImage: AppIconType.uptrend_Chart.image)
+        }
+        
+        coninInfoVC.do {
+            $0.tabBarItem = UITabBarItem(title: StringLiterals.TabBar.coinInfoTitle,
+                                         image: AppIconType.chart_Bar.image,
+                                         selectedImage: AppIconType.chart_Bar.image)
+        }
+        
+        portfolioVC.do {
+            $0.tabBarItem = UITabBarItem(title: StringLiterals.TabBar.portfolioTitle,
+                                         image: AppIconType.star.image,
+                                         selectedImage: AppIconType.star_fill.image)
+        }
+        
+        setViewControllers([exchangeVC, coninInfoVC, portfolioVC], animated: true)
+        
+        self.selectedIndex = 0
+    }
+    
+    private func setTabBarAppearence () {
+        let appearence = UITabBarAppearance ()
+        appearence.configureWithTransparentBackground()
+        appearence.backgroundColor = UIColor.bg
+        tabBar.standardAppearance = appearence
+        tabBar.scrollEdgeAppearance = appearence
+        tabBar.tintColor = UIColor.primary
+    }
+    
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    
+    //탭바 아이템 선택으로인한 화면전환 시 깜빡임 방지
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if let fromView = selectedViewController?.view, let toView = viewController.view, fromView != toView {
+            UIView.transition(from: fromView, to: toView, duration: 0.0, options: [], completion: nil)
+        }
+        return true
+    }
+    
+}
+
+
+
+//#Preview {
+//        let vc = TabBarViewController()
+//        return vc
+//}
---
+++ Het-Je-iOS/Het-Je-iOS/Presentation/4. 포트폴리오/Views/OtherViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OtherViewController.swift
+//  Het-Je-iOS
+//
+//  Created by 박신영 on 3/7/25.
+//
+
+import UIKit
+
+final class OtherViewController: BaseViewController {
+    
+    override func setStyle() {
+        view.backgroundColor = .brown
+    }
+    
+}
