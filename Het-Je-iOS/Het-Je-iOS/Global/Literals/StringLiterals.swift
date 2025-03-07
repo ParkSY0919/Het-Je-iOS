5ced2ab54ea4027815830a304c4d7180f24f6915
ParkSY0919 <qhr498@gmail.com>
2025-03-07 17:22:08 +0900

    [Feat/#3] StringLiterals TabBar 추가

--- Het-Je-iOS/Het-Je-iOS/Global/Literals/StringLiterals.swift
+++ Het-Je-iOS/Het-Je-iOS/Global/Literals/StringLiterals.swift
@@ -11,5 +11,10 @@ enum StringLiterals {}
 
 extension StringLiterals {
     
+    enum TabBar {
+        static let exchangeTitle = "거래소"
+        static let coinInfoTitle = "코인정보"
+        static let portfolioTitle = "포트폴리오"
+    }
     
 }
