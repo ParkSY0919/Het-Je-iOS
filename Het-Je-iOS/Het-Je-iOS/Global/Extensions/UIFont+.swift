8e9d37b567e68806fd74a5d99daaaf742fd93491
ParkSY0919 <qhr498@gmail.com>
2025-03-07 15:31:24 +0900

    [Setting/#1] color, image, font 세팅

---
+++ Het-Je-iOS/Het-Je-iOS/Global/Extensions/UIFont+.swift
@@ -0,0 +1,41 @@
+//
+//  UIFont+.swift
+//  Het-Je-iOS
+//
+//  Created by 박신영 on 3/7/25.
+//
+
+import UIKit
+
+extension UIFont {
+    enum FontName {
+        case body_bold_12
+        case body_bold_9
+        case body_regular_12
+        case body_regular_9
+         
+        var fontWeight: UIFont.Weight {
+            switch self {
+            case .body_bold_12, .body_bold_9:
+                return .bold
+            case .body_regular_12, .body_regular_9:
+                return .regular
+            }
+        }
+        
+        var size: CGFloat {
+            switch self {
+            case .body_bold_12, .body_regular_12:
+                return 12
+            case .body_bold_9, .body_regular_9:
+                return 9
+            }
+        }
+    }
+    
+    static func hetJeFont(_ style: FontName) -> UIFont {
+        return UIFont.systemFont(ofSize: style.size, weight: style.fontWeight)
+    }
+    
+}
+
