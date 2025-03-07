6fd58d32d2b27a5d514afa559a902ce1110e31fb
ParkSY0919 <qhr498@gmail.com>
2025-03-08 03:44:46 +0900

    [Feat/#7] fix formatNum(), feat formatAddTradePrice()

--- Het-Je-iOS/Het-Je-iOS/Global/Utility/CustomFormatterManager.swift
+++ Het-Je-iOS/Het-Je-iOS/Global/Utility/CustomFormatterManager.swift
@@ -35,15 +35,39 @@ final class CustomFormatterManager {
         return outputDate.string(from: date)
     }
     
-    func formatNum(num: Double) -> String {
-        //정수라면
-        if num.truncatingRemainder(dividingBy: 1) == 0 {
-            let formatter = NumberFormatter()
-            formatter.numberStyle = .decimal
-            return formatter.string(from: NSNumber(value: Int(num))) ?? "formatNum fail"
+    func formatNum(num: Double, isTradePrice: Bool = false) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 2
+        
+        let formatted = formatter.string(from: NSNumber(value: num)) ?? "formatNum fail"
+        
+        //isTradePrice == true, .(소수점) 있으면서, 접미사 0인지 조건부 처리
+        if isTradePrice, let decimalIndex = formatted.firstIndex(of: ".") {
+            //접미사 "0"인지
+            if formatted.hasSuffix("0") {
+                //소수점과 그 다음 숫자까지만 남기고 자름
+                  //after: 주어진 index 바로 뒤까지 반환
+                let trimmed = String(formatted.prefix(upTo: formatted.index(after: decimalIndex)))
+                return trimmed
+            }
+        }
+        return formatted
+    }
+
+
+    func formatAddTradePrice(num: Double) -> String {
+        let million = 1000000.0
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 0
+        
+        if num >= million {
+            let formattedValue = formatter.string(from: NSNumber(value: num / million)) ?? "0"
+            return "\(formattedValue)백만"
         } else {
-            //소수라면
-            return String(format: "%.2f", num)
+            return formatter.string(from: NSNumber(value: num)) ?? "0"
         }
     }
     
