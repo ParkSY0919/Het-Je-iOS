5babbf77269ab82bd86d453c8afaa961606581a3
ParkSY0919 <qhr498@gmail.com>
2025-03-08 23:21:00 +0900

    [Add/#9] TrendingAPI Response Model 추가

---
+++ Het-Je-iOS/Het-Je-iOS/Global/Network/TrendingAPI/ResponseModel/TrendingAPIResponseModel.swift
@@ -0,0 +1,66 @@
+//
+//  TrendingAPIResponseModel.swift
+//  Het-Je-iOS
+//
+//  Created by 박신영 on 3/8/25.
+//
+
+extension DTO.Response {
+    
+    struct TrendingAPIResponseModel: Codable {
+        let coins: [Coin]
+        let nfts: [Nft]
+    }
+    
+    struct Coin: Codable {
+        let item: TrendingCoinDetails
+    }
+    
+    struct TrendingCoinDetails: Codable {
+        let name, symbol: String
+        let marketCapRank: Int
+        let thumb, small, large: String
+        let data: TrendingCoinData
+        
+        enum CodingKeys: String, CodingKey {
+            case name, symbol
+            case marketCapRank = "market_cap_rank"
+            case thumb, small, large
+            case data
+        }
+    }
+    
+    struct TrendingCoinData: Codable {
+        let priceChangePercentage24H: [String: Double]
+
+        enum CodingKeys: String, CodingKey {
+            case priceChangePercentage24H = "price_change_percentage_24h"
+        }
+    }
+    
+    struct Nft: Codable {
+        let name, symbol: String
+        let thumb: String
+        let nativeCurrencySymbol: String
+        let floorPriceInNativeCurrency: Double
+        let floorPrice24HPercentageChange: Double
+        let data: TrendingNFTData
+        
+        enum CodingKeys: String, CodingKey {
+            case name, symbol, thumb
+            case nativeCurrencySymbol = "native_currency_symbol"
+            case floorPriceInNativeCurrency = "floor_price_in_native_currency"
+            case floorPrice24HPercentageChange = "floor_price_24h_percentage_change"
+            case data
+        }
+    }
+    
+    struct TrendingNFTData: Codable {
+        let floorPriceInUsd24HPercentageChange: String
+        
+        enum CodingKeys: String, CodingKey {
+            case floorPriceInUsd24HPercentageChange = "floor_price_in_usd_24h_percentage_change"
+        }
+    }
+        
+}
