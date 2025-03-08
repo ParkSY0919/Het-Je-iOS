5c5411587cfb55fac9cd3f36dc5432be26e6e0c2
ParkSY0919 <qhr498@gmail.com>
2025-03-09 02:31:36 +0900

    [Feat/#13] 코인정보 화면 SearchTextField UI 구현

--- Het-Je-iOS/Het-Je-iOS/Presentation/3. 코인정보(CoinInfo)/Views/CoinInfoViewController.swift
+++ Het-Je-iOS/Het-Je-iOS/Presentation/3. 코인정보(CoinInfo)/Views/CoinInfoViewController.swift
@@ -14,10 +14,59 @@ import Then
 
 final class CoinInfoViewController: BaseViewController {
     
+    private let searchTextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setNav()
+    }
+    
+    override func setHierarchy() {
+        view.addSubviews(searchTextField)
+    }
+    
+    override func setLayout() {
+        searchTextField.snp.makeConstraints {
+            $0.top.equalTo(underLine.snp.bottom).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+            $0.height.equalTo(45)
+        }
+    }
+    
+    override func setStyle() {
+        let containerView = UIView()
+        let searchImageView = UIImageView()
+        containerView.addSubview(searchImageView)
+        
+        containerView.snp.makeConstraints {
+            $0.width.equalTo(40)
+            $0.height.equalTo(45)
+        }
+        
+        searchImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview().offset(5)
+            $0.centerY.equalToSuperview()
+        }
+        
+        searchImageView.do {
+            $0.image = AppIconType.search.image
+            $0.tintColor = .secondary
+            $0.contentMode = .scaleAspectFill
+        }
+        searchTextField.do {
+            $0.leftView = containerView
+            $0.leftViewMode = .always
+            $0.setPlaceholder(
+                placeholder: "검색어를 입력해주세요.",
+                fontColor: .secondary,
+                font: .systemFont(ofSize: 14, weight: .bold) //주어진 12로 하기에는 글자가 작아 변경
+            )
+            $0.font = .systemFont(ofSize: 14, weight: .bold)
+            $0.layer.borderWidth = 1.5
+            $0.layer.borderColor = UIColor.secondary.cgColor
+            $0.layer.cornerRadius = 45/2
+        }
     }
     
 }
