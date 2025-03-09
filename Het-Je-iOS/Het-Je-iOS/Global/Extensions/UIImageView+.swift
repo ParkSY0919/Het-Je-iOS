de84a5f555b77d3451cd4c642d856c85c5996277
ParkSY0919 <qhr498@gmail.com>
2025-03-09 12:39:23 +0900

    [Feat/#13] UIImageView+ 함수 추가

--- Het-Je-iOS/Het-Je-iOS/Global/Extensions/UIImageView+.swift
+++ Het-Je-iOS/Het-Je-iOS/Global/Extensions/UIImageView+.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-import SnapKit
+import Kingfisher
 
 extension UIImageView {
     
@@ -18,5 +18,41 @@ extension UIImageView {
         self.image = image
         self.contentMode = .scaleAspectFill
     }
+    
+    //Downsampling 기능 활용하여 메모리 누수 방지
+    func setImageKfDownSampling(with urlString: String?, cornerRadius: Int) {
+        var url = ""
+        switch urlString == "" || urlString == nil {
+        case true:
+            self.setEmptyImageView()
+        case false:
+            guard let urlString else { return }
+            url = urlString
+            
+            let processor = DownsamplingImageProcessor(size: self.bounds.size)
+            self.kf.indicatorType = .activity
+            self.kf.setImage(
+                with: URL(string: url),
+                placeholder: UIImage(named: "placeholderImage"),
+                options: [
+                    .processor(processor),
+                    .scaleFactor(UIScreen.main.scale),
+                    .transition(.fade(1)),
+                    .cacheOriginalImage
+                ]
+            )
+            self.clipsToBounds = true
+            self.contentMode = .scaleAspectFill
+            self.layer.cornerRadius = CGFloat(cornerRadius)
+        }
+    }
+    
+    func setEmptyImageView(imageStr: String? = "xmark.bin") {
+        self.image = UIImage(systemName: imageStr ?? "xmark.bin")
+        self.clipsToBounds = true
+        self.contentMode = .scaleAspectFit
+        self.tintColor = UIColor.lightGray
+    }
+
     
 }
