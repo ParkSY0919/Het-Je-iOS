e07ba2835e6469cc95713b4073e8125e815221fa
ParkSY0919 <qhr498@gmail.com>
2025-03-11 19:39:05 +0900

    [Feat/#21] 네트워크 단절 상황 및 재개 대응 UI 및 기능 구현

--- Het-Je-iOS/Het-Je-iOS/Presentation/Base/BaseViewController.swift
+++ Het-Je-iOS/Het-Je-iOS/Presentation/Base/BaseViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
+import Network
 import SnapKit
 import Then
 import Toast
 
 class BaseViewController: UIViewController {
+    
+    private let monitor = NWPathMonitor()
+    private var alreadyPresent = false
     
     let underLine = UIView()
     
@@ -28,6 +32,7 @@ class BaseViewController: UIViewController {
         setLayout()
         setStyle()
         view.backgroundColor = .white
+        startMonitoring()
     }
     
     private func setUnderLine() {
@@ -90,13 +95,14 @@ class BaseViewController: UIViewController {
         }
     }
     
-    func showToast(message: String) {
+    func showToast(message: String, isNetworkToast: Bool = false) {
         var style = ToastStyle()
         style.backgroundColor = .secondary
         style.titleColor = .primary
         style.cornerRadius = 10
         
-        view.makeToast(message, duration: 2.5, position: .bottom, style: style)
+        let duration: Double = isNetworkToast ? 1 : 2.5
+        view.makeToast(message, duration: duration, position: .bottom, style: style)
     }
     
     @objc
@@ -109,9 +115,59 @@ class BaseViewController: UIViewController {
         print(#function)
     }
     
+    func checkNetworkStatus() -> Bool {
+        return monitor.currentPath.status == .satisfied
+    }
+        
+    func resetMonitoring() {
+        alreadyPresent = false
+    }
+    
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
 }
+
+private extension BaseViewController {
+    
+    func startMonitoring() {
+        monitor.start(queue: .global())
+        monitor.pathUpdateHandler = { [weak self] path in
+            guard let self = self else { return }
+            
+            DispatchQueue.main.async {
+                //이미 alert이 표시되고 있다면 추가 동작 X
+                if self.alreadyPresent {
+                    return
+                }
+                
+                if path.status == .satisfied {
+                    print("인터넷 연결 상태 양호: 알림 닫힘")
+                } else {
+                    // 인터넷 연결이 없을 때 알림 표시 (중복 표시 방지)
+                    self.showNetworkAlert()
+                }
+            }
+        }
+    }
+    
+    func showNetworkAlert() {
+        // testViewController가 표시 중이면 중복 표시하지 않음
+        if let presentedVC = self.presentedViewController, presentedVC is NetworkGuidanceViewController {
+            print("이미 네트워크 알림이 표시 중입니다.")
+            return
+        }
+        
+        let vc = NetworkGuidanceViewController()
+        // Pass a reference to this controller for the retry action
+        vc.baseViewController = self
+        viewTransition(viewController: vc, transitionStyle: .overCurrentContext)
+        // Pause automatic monitoring when showing alert
+        alreadyPresent = true
+        print("네트워크 알림 표시됨")
+    }
+    
+}
+
---
+++ Het-Je-iOS/Het-Je-iOS/Presentation/네트워크 알림/Views/NetworkGuidanceViewController.swift
@@ -0,0 +1,137 @@
+//
+//  NetworkGuidanceViewController.swift
+//  Het-Je-iOS
+//
+//  Created by 박신영 on 3/11/25.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+final class NetworkGuidanceViewController: UIViewController {
+    
+    //baseVC 내부 모니터와 함수에 접근해야 하기에 delegate 받아옴
+    weak var baseViewController: BaseViewController?
+    private let viewModel = NetworkGuidanceViewModel()
+    private let disposeBag = DisposeBag()
+    
+    private let alertBoxTopView = UIView()
+    private let guideLabel = UILabel()
+    private let statusLabel = UILabel()
+    
+    private let underLineView = UIView()
+    
+    private let alertBoxBottomView = UIView()
+    private let retryButton = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setHierarchy()
+        setLayout()
+        setStyle()
+        bind()
+    }
+    
+    private func setHierarchy() {
+        view.addSubviews(
+            alertBoxTopView,
+            underLineView,
+            alertBoxBottomView
+        )
+        
+        alertBoxTopView.addSubviews(guideLabel, statusLabel)
+        alertBoxBottomView.addSubview(retryButton)
+    }
+    
+    private func setLayout() {
+        alertBoxTopView.snp.makeConstraints {
+            $0.centerX.equalTo(view.safeAreaLayoutGuide)
+            $0.centerY.equalTo(view.safeAreaLayoutGuide).offset(-20)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(50)
+            $0.height.equalTo(100)
+        }
+        
+        guideLabel.snp.makeConstraints {
+            $0.top.equalTo(alertBoxTopView.safeAreaLayoutGuide).offset(20)
+            $0.centerX.equalTo(alertBoxTopView.safeAreaLayoutGuide)
+        }
+        
+        statusLabel.snp.makeConstraints {
+            $0.top.equalTo(guideLabel.snp.bottom).offset(20)
+            $0.horizontalEdges.equalTo(alertBoxTopView.safeAreaLayoutGuide).inset(20)
+        }
+        
+        underLineView.snp.makeConstraints {
+            $0.top.equalTo(alertBoxTopView.snp.bottom)
+            $0.horizontalEdges.equalTo(alertBoxTopView.snp.horizontalEdges)
+            $0.height.equalTo(1)
+        }
+        
+        alertBoxBottomView.snp.makeConstraints {
+            $0.top.equalTo(underLineView.snp.bottom)
+            $0.horizontalEdges.equalTo(alertBoxTopView.snp.horizontalEdges)
+            $0.height.equalTo(50)
+        }
+        
+        retryButton.snp.makeConstraints {
+            $0.center.equalTo(alertBoxBottomView.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func setStyle() {
+        alertBoxTopView.backgroundColor = .white
+        alertBoxBottomView.backgroundColor = .white
+        underLineView.backgroundColor = .bg
+        
+        guideLabel.setLabelUI(
+            "안내",
+            font: .hetJeFont(.body_bold_12),
+            textColor: .primary,
+            alignment: .center
+        )
+        
+        statusLabel.setLabelUI(
+            "네트워크 연결이 일시적으로 원활하지 않습니다. 데이터 또는 Wi-Fi 연결 상태를 확인해주세요.",
+            font: .hetJeFont(.body_regular_12),
+            textColor: .primary,
+            alignment: .center,
+            numberOfLines: 0
+        )
+        
+        retryButton.do {
+            $0.setTitle("다시 시도하기", for: .normal)
+            $0.setTitleColor(.primary, for: .normal)
+            $0.titleLabel?.font = .hetJeFont(.body_bold_12)
+        }
+    }
+    
+    private func bind() {
+        let input = NetworkGuidanceViewModel.Input(
+            in_retryButtonTapped: retryButton.rx.controlEvent(.touchUpInside)
+        )
+        
+        let output = viewModel.transform(input: input)
+        
+        output.out_retryButtonTapped
+            .bind(with: self) { owner, _ in
+                if let baseVC = owner.baseViewController {
+                    if baseVC.checkNetworkStatus() {
+                        print("인터넷 연결 확인")
+                        owner.dismiss(animated: true) {
+                            baseVC.resetMonitoring()
+                        }
+                    } else {
+                        print("인터넷 연결 실패")
+                        baseVC.showToast(message: "네트워크 통신이 원활하지 않습니다.", isNetworkToast: true)
+                    }
+                }
+            }.disposed(by: disposeBag)
+    }
+    
+}
+
